82c49f0017cd3a4b60c032498455f17482c2fe3d
laniakea42 <mintlol@mail.ru>
2022-02-21 10:28:29 +0300

    init

---
+++ app/js/common.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	$('.pixels_slider').slick({
+		speed: 1,
+		dots: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplaySpeed: 600,
+		arrows: false,
+		autoplay: true,
+		fade: true,
+		draggable: false
+	})
+
+
+});
